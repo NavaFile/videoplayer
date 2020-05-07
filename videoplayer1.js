cbb52b18dbaff43e9702bfbccc7d179b2da0a8ff
nava <nava.chaithu@hotmail.com>
2020-05-07 19:45:27 +0530

    nava.nvest@gmail.com

--- videoplayer1.js
+++ videoplayer1.js
@@ -1,37 +1,46 @@
 function LoadCryptoData() {
-    console.log("LOAd cryptoo data");
+  console.log("LOAd cryptoo data");
 
-    axios({
-        url: "https://comms.globalxchange.com/coin/promo/farm/payout/quote/get?campaign_id=gxcom_gxt&video_no=1",
-        method: "get",
-      
+  axios({
+    url:
+      "https://comms.globalxchange.com/coin/promo/farm/payout/quote/get?campaign_id=gxcom_gxt&video_no=1",
+    method: "get",
+  })
+    .then((result) => {
+      console.log(result.data);
 
-    }).then(result => {
-        console.log(result.data);
-
-        console.log('The Crypto Data is :', result.data.status)
-        document.getElementById ('payout').innerHTML = result.data.payout.toFixed(2)
-        document.getElementById ('coin').innerHTML =result.data.coin
-    }).catch(error => {
-        console.log('Error Loading Cryoto Data')
+      console.log("The Crypto Data is :", result.data.status);
+      document.getElementById("payout").innerHTML = result.data.payout.toFixed(
+        2
+      );
+      document.getElementById("coin").innerHTML = result.data.coin;
     })
+    .catch((error) => {
+      console.log("Error Loading Cryoto Data");
+    });
 }
 
-LoadCryptoData()
+LoadCryptoData();
 
 
-axios({
-    method: 'POST',
-    url: 'https://comms.globalxchange.com/coin/promo/farm/payout/claim',
-    data: {
-        email: email,
-        campaign_id: campaign_id,
-        video_no: 'videoNumber',
-        video_title: 'coin',
-        payout_amount_given:'payout'
-    }
-  }).then(function() {
-    console.log("done");
-  });
+document.getElementById('closemodal').onclick = function postgxclaim() {
+  console.log("postgxclaim");
+  axios
+    .post("https://comms.globalxchange.com/coin/promo/farm/payout/claim", 
+    {
+      email: email,
+      campaign_id: campaign_id,
+      video_no: "videoNumber",
+      video_title: "coin",
+      payout_amount_given: "payout",
+    })
+    .then(function () {
+      console.log("done");
+    });
+};
+postgxclaim();
+
+
+
 
 
