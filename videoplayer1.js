581f8040e4164879bb63dbe4c691bd43cae0b82c
nava <nava.chaithu@hotmail.com>
2020-05-08 12:49:37 +0530

    api post

--- videoplayer1.js
+++ videoplayer1.js
@@ -1,3 +1,5 @@
+// Get Method It IS Working
+
 function LoadCryptoData() {
   console.log("LOAd cryptoo data");
 
@@ -10,9 +12,9 @@ function LoadCryptoData() {
       console.log(result.data);
 
       console.log("The Crypto Data is :", result.data.status);
-      document.getElementById("payout").innerHTML = result.data.payout.toFixed(
-        2
-      );
+      document.getElementById(
+        "payout"
+      ).innerHTML = result.data.payout.toFixed();
       document.getElementById("coin").innerHTML = result.data.coin;
     })
     .catch((error) => {
@@ -22,25 +24,25 @@ function LoadCryptoData() {
 
 LoadCryptoData();
 
+// Post
 
-document.getElementById('closemodal').onclick = function postgxclaim() {
-  console.log("postgxclaim");
-  axios
-    .post("https://comms.globalxchange.com/coin/promo/farm/payout/claim", 
-    {
-      email: email,
-      campaign_id: campaign_id,
-      video_no: "videoNumber",
-      video_title: "coin",
-      payout_amount_given: "payout",
-    })
-    .then(function () {
-      console.log("done");
-    });
+
+window.onload = function () {
+  document.getElementById("closemodal").onclick = function () {
+    
+    console.log("postgxclaim");
+    axios
+      .post("https://comms.globalxchange.com/coin/promo/farm/payout/claim", {
+        email: localStorage.getItem("email"),
+        
+        campaign_id: "gxcom_gxt",
+        video_no: "videoNumber",
+        video_title: "coin",
+        payout_amount_given: "payout",
+      })
+      .then(function () {
+        console.log("done");
+      });
+  };
 };
-postgxclaim();
-
-
-
-
-
+// postgxclaim();
